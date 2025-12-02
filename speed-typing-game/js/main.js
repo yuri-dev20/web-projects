2673da2b60ba267e6c93fed29f664b3b8f97ab85
yuri-dev20 <yuriglauco945@gmail.com>
2025-12-02 15:16:05 -0300

    speed typing game - game ui start

--- speed-typing-game/js/main.js
+++ speed-typing-game/js/main.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!")
+const startBtn = document.querySelector("#game-start-btn");
+const textDisplay = document.querySelector("#text-display");
+const timeDisplay = document.querySelector("#time-display");
+const playerInput = document.querySelector("#player-input");
+const timeScores = document.querySelector("#time-scores");
+
+startBtn.addEventListener("click", (e) => {
+    // Make the game ui appear
+    textDisplay.classList.remove("invisible");
+    timeDisplay.classList.remove("invisible");
+    playerInput.classList.remove("invisible");
+    timeScores.classList.remove("invisible");
+
+    // Make the button disappear
+    startBtn.classList.add("invisible");
+});

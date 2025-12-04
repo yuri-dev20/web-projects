5c2b65e97f454435841db2c302ab8beda39b77b3
yuri-dev20 <yuriglauco945@gmail.com>
2025-12-04 16:37:22 -0300

    simon game - refactor entire game logic and small tweaks on html

--- simon-game/js/main.js
+++ simon-game/js/main.js
@@ -7,7 +7,12 @@ const btnBlue = document.querySelector(".blue");
 const btnYellow = document.querySelector(".yellow");
 
 const colors = ["red", "green", "blue", "yellow"];
-let color;
+
+let userTurn = false;
+
+// computerChoices is the sequence to be follow by the user and the userChoice is the index that show success or error
+let userChoices = 0;
+const computerChoices = [];
 
 // This just makes the div flash to solve the problem when the color repeats
 function flashColor(btn, clsName) {
@@ -18,52 +23,70 @@ function flashColor(btn, clsName) {
     }, 300);
 }
 
-function game() {
-    color = colors[Math.floor(Math.random() * 4)];
-
-    // Remove each class before choosing again for control
-    gameColors.forEach(btn => {
-        btn.classList.remove("red-choice", "green-choice", "blue-choice", "yellow-choice");
-    });
-    
+function getButtonByColor(color) {
     switch (color) {
-        case "red":
-            flashColor(btnRed, "red-choice");
-            console.log("RED")
-            break;
-        
-        case "green":
-            flashColor(btnGreen, "green-choice");
-            console.log("GREEN")
-            break;
-    
-        case "blue":
-            flashColor(btnBlue, "blue-choice");
-            console.log("BLUE")
-            break;
-    
-        case "yellow":
-            flashColor(btnYellow, "yellow-choice");
-            console.log("YELLOW")
-            break;
-    };
+        case "red": return btnRed;
+        case "green": return btnGreen;
+        case "blue": return btnBlue;
+        case "yellow": return btnYellow;
+    }
 }
 
-function gameLoop() {
-    for (let i = 1; i < 10; i +=2) {
-        // Calls the game function after a delay:
-        // i = 1 - 1 * 1000
-        // i = 3 - 3 * 1000
-        // i = 5 - 5 * 1000
-        // i = 7 - 7 * 1000
-        // i = 9 - 9 * 1000
-        setTimeout(game, i * 1000);    
+function playTurn(i = 0) {
+    if (i >= computerChoices.length) {
+        userTurn = true;
+        userChoices = 0;
+        return;
     }
 
-    // Reset
-    gameColors.forEach(btn => {
-        btn.classList.remove("red-choice", "green-choice", "blue-choice", "yellow-choice");
-    });
+    const color = computerChoices[i];
+    const btn = getButtonByColor(color)
+
+    //  'red(color)-choice'
+    flashColor(btn, color + "-choice");
+
+    setTimeout(() => {
+        playTurn(i + 1); // call the next turn/color
+    }, 600);
 }
 
-gameLoop();
+// Adds a new color since the idea is the computer to be the one in control
+function addColor() {
+    computerChoices.push(colors[Math.floor(Math.random() * 4)]);
+}
+
+// User turn and when it interacts
+function userClick(e) {
+    // Ignore user clicks while the game shows the colors
+    if (!userTurn) {
+        return;
+    }
+
+    const clicked = e.target.id;
+
+    if (clicked !== computerChoices[userChoices]) {
+        console.log("Wrong color - GAME OVER!");
+        playTurn = false;
+        return;
+    }
+
+    userChoices++;
+
+    if (userChoices === computerChoices.length) {
+        console.log("YOU MADE IT - NEXT ROUND!");
+        userTurn = false;
+
+        setTimeout(startRound, 1000);
+    }
+}
+
+function startRound() {
+    addColor();
+    playTurn();
+}
+
+gameColors.forEach(clrBtn => {
+    clrBtn.addEventListener("click", userClick);
+});
+
+startRound();

bf86ae35117d491681dd0883ed39ffeb0991fe3d
yuri-dev20 <yuriglauco945@gmail.com>
2026-01-06 19:08:15 -0300

    random password generator - app logic implemented

--- random-password-generator/js/main.js
+++ random-password-generator/js/main.js
@@ -1,21 +1,23 @@
 const btns = document.querySelectorAll("#btns");
 
 const explanationDiv = document.querySelector(".btn-explanation");
+const generatePassBtn = document.querySelector(".generate-pass");
 const passwordInput = document.querySelector("#password");
 
+let chosenLevel = "";
 const passwordLevels = {
     "simple": {
-        "description": "A simple password is short lenght with 8 lowercase and uppercase letters",
+        "description": "A simple password is has a lenght of 8 lowercase and uppercase letters",
         "letters": "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
     },
     "moderate": {
-        "description": "A moderate password is of moderate length 9 to 12 characters and includes a mix of lowercase and uppercase letters, numbers, and at least one symbol",
+        "description": "A moderate password has a length of 12 characters and includes a mix of lowercase and uppercase letters, numbers, and at least one symbol",
         "letters": "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
         "numbers": "0123456789",
         "symbols": "!@#$%^&*()_+-",
     },
     "strong": {
-        "description": "A strong password is long 13 to 20 characters and includes a mix of lowercase and uppercase letters, numbers, and multiple symbols, avoiding common words or predictable patterns",
+        "description": "A strong password is 18 characters long and includes a mix of lowercase and uppercase letters, numbers, and multiple symbols, avoiding common words or predictable patterns",
         "letters": "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
         "numbers": "0123456789",
         "symbols": "!@#$%^&*()_+-",
@@ -27,6 +29,7 @@ btns.forEach(btn => {
     btn.addEventListener("click", (e) => {
         
         explanationDiv.classList.remove("disable");
+        generatePassBtn.classList.remove("disable");
         explanationDiv.innerHTML = "";
 
         const explanationTitle = document.createElement("h3");
@@ -39,6 +42,7 @@ btns.forEach(btn => {
 
             if (passwordLevel === e.target.textContent.toLowerCase()) {
                 description.textContent = passwordOptions["description"];
+                chosenLevel = passwordLevel;
             }
         
         }
@@ -47,3 +51,40 @@ btns.forEach(btn => {
         explanationDiv.appendChild(description);
     });
 });
+
+generatePassBtn.addEventListener("click", () => {
+    let generatedPass = "";
+    for (const [passwordLevel, passwordOptions] of Object.entries(passwordLevels)) {
+
+        if (chosenLevel === "simple") {
+            generatedPass = "";
+            const passEnconde = passwordOptions["letters"];
+            generatedPass = randomPassword(passEnconde, 8);
+            passwordInput.textContent = generatedPass;
+
+        } else if (chosenLevel === "moderate") {
+            generatedPass = "";
+            const passEnconde = passwordOptions["letters"] + passwordOptions["numbers"] + passwordOptions["symbols"];
+            generatedPass += randomPassword(passEnconde, 12);
+            passwordInput.textContent = generatedPass;
+
+        } else if (chosenLevel === "strong") {
+            generatedPass = "";
+            const passEnconde = passwordOptions["letters"] + passwordOptions["numbers"] + passwordOptions["symbols"];
+            generatedPass += randomPassword(passEnconde, 18);
+            passwordInput.textContent = generatedPass;
+    }
+}});
+
+function randomPassword(characters, size) {
+    let result = "";
+
+    for (let i = 0; i < size; i++) {
+
+        const randIndex = Math.floor(Math.random() * characters.length);
+        const randLetter = characters[randIndex];
+        result += randLetter;
+    }
+
+    return result;
+}
